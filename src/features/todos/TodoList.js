8b7e98d1a8b3a7a3c0a5f97cd55a93ddc71429c6
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-07-08 16:40:45 +0430

    Update styles with TailwindCSS

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -4,13 +4,12 @@ import {
   useDeleteTodoMutation,
   useAddTodoMutation,
 } from "../api/apiSlice";
-import { FaUpload } from "react-icons/fa";
-import { FaTrashAlt } from "react-icons/fa";
-
+import { FaUpload, FaTrashAlt, FaEdit, FaPencilAlt } from "react-icons/fa";
 import { useState } from "react";
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
+  const [editedTodo, setEditedTodo] = useState(null);
 
   const {
     data: todos,
@@ -29,29 +28,76 @@ const TodoList = () => {
     setNewTodo("");
   };
 
+  //editTodo
+  const handleEditSubmit = (e, editedTodo) => {
+    e.preventDefault();
+    updateTodo({
+      id: editedTodo.id,
+      title: editedTodo.title,
+      completed: editedTodo.completed,
+    });
+    setEditedTodo(null);
+  };
+
   const newItemSection = (
-    <form
-      onSubmit={handleSubmit}
-      className="p-4 flex justify-between items-center border border-solid border-gray-700 mb-4"
-    >
-      <label className="absolute left-[-100000px]" htmlFor="new-todo">
-        Enter a new todo item
-      </label>
-      <div className="new-todo">
-        <input
-          className="w-full pr-[30px] p-2 rounded-md border border-solid border-gray-700 focus:bg-green-300 submit-hover:bg-green-300"
-          type="text"
-          id="new-todo"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Enter new todo"
-        />
-      </div>
-      <button className="min-w-50 min-h-50 text-blue-600">
-        <FaUpload />
-      </button>
-    </form>
+    <div className="flex justify-center mt-[6%]">
+      <form
+        onSubmit={handleSubmit}
+        className="w-1/2 p-4 flex justify-between items-center border border-solid border-gray-400 mb-4 rounded-xl shadow-md"
+      >
+        <label className="absolute left-[-100000px] " htmlFor="new-todo">
+          Enter a new todo item
+        </label>
+        <div className="new-todo">
+          <input
+            className="pr-[30px] p-2 !outline-none "
+            type="text"
+            id="new-todo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Enter new todo"
+          />
+        </div>
+        <button className="text-2xl text-yellow-600">
+          <FaUpload />
+        </button>
+      </form>
+    </div>
   );
+
+  //editTodo
+  let editedTodoSection;
+  if (editedTodo !== null) {
+    editedTodoSection = (
+      <div className="flex justify-center">
+        <form
+          onSubmit={(e) => handleEditSubmit(e, editedTodo)}
+          className="w-1/2 p-4 flex justify-between items-center border border-solid border-gray-700 mb-4 rounded-xl shadow-md"
+        >
+          <label className="absolute left-[-100000px]" htmlFor="edit-todo">
+            Edit todo item
+          </label>
+          <div className="edit-todo">
+            <input
+              className="pr-[30px] p-2 !outline-none"
+              type="text"
+              id="edit-todo"
+              value={editedTodo.title}
+              onChange={(e) =>
+                setEditedTodo({ ...editedTodo, title: e.target.value })
+              }
+              placeholder="Edit todo"
+            />
+          </div>
+          <button className="min-w-50 min-h-50 text-red-600">
+            <FaPencilAlt />
+          </button>
+        </form>
+      </div>
+    );
+  } else {
+    editedTodoSection = null;
+  }
 
   let content;
   if (isLoading) {
@@ -61,28 +107,38 @@ const TodoList = () => {
       //  JSON.stringify(todos);
 
       return (
-        <article
-          key={todo.id}
-          className="font-nunito text-lg p-4 flex justify-between items-center border border-gray-400"
-        >
-          <div className="flex justify-start items-center">
-            <input
-              className="min-w-30 min-h-30 mr-4"
-              type="checkbox"
-              id={todo.id}
-              onChange={() =>
-                updateTodo({ ...todo, completed: !todo.completed })
-              }
-            />
-            <label htmlFor={todo.id}>{todo.title}</label>
-          </div>
-          <button
-            className="min-w-50 min-h-50 rounded-10 cursor-pointer bg-white text-pink-400 focus:filter brightness-120 trash-hover:filter brightness-120"
-            onClick={() => deleteTodo({ id: todo.id })}
+        <div className="flex justify-center font-nunito">
+          <article
+            key={todo.id}
+            className="w-1/2 font-nunito text-lg p-4 flex  border border-gray-400 rounded-xl shadow-md mb-5 relative"
           >
-            <FaTrashAlt />
-          </button>
-        </article>
+            <div className="flex  ">
+              <input
+                type="checkbox"
+                className="mr-4 bg-red-500 border-red-300 text-red-500  cursor-pointer"
+                id={todo.id}
+                onChange={() =>
+                  updateTodo({ ...todo, completed: !todo.completed })
+                }
+              />
+              <label htmlFor={todo.id}>{todo.title}</label>
+            </div>
+            <div className="absolute right-4 space-x-3">
+              <button
+                className="bg-white text-yellow-900 "
+                onClick={() => deleteTodo({ id: todo.id })}
+              >
+                <FaTrashAlt />
+              </button>
+              <button
+                className=" bg-white text-green-700"
+                onClick={() => setEditedTodo(todo)}
+              >
+                <FaEdit />
+              </button>
+            </div>
+          </article>
+        </div>
       );
     });
   } else if (isError) {
@@ -91,8 +147,11 @@ const TodoList = () => {
 
   return (
     <main>
-      <h1>Todo List</h1>
+      <h1 className="text-center font-nunito font-bold text-4xl text-yellow-500 mt-[4%] drop-shadow-md">
+        Todo List 📝
+      </h1>
       {newItemSection}
+      {editedTodoSection}
       {content}
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+    },
   },
   plugins: [],
 };
